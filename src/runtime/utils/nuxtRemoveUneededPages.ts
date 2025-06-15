0439fee176b04ea9814cc8d4a373734326f9e63f
Alan North <alanscodelog@gmail.com>
2025-06-15 11:50:44 -0300

    v0breaking: improved nuxtRemoveUneededPages
    It no longer needs to be wrapped in the 'pages:extend' hook, it's now done
    automatically.

--- src/runtime/utils/nuxtRemoveUneededPages.ts
+++ src/runtime/utils/nuxtRemoveUneededPages.ts
@@ -1,14 +1,17 @@
-import { type NuxtPage } from "nuxt/schema"
+import { type Nuxt, type NuxtPage } from "nuxt/schema"
 
 /** Remove extraneous unneeded routes from the build, only keep given routes. */
-export function nuxtRemoveUneededPages(pages: NuxtPage[], routesToInclude: string[]) {
-	const toRemove = []
-	for (const page of pages) {
-		const isWanted = routesToInclude.some(route => route === page.path)
-		if (!isWanted) {
-			toRemove.push(page)
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function nuxtRemoveUneededPages(nuxt: Nuxt, routesToInclude: string[]) {
+	nuxt.hook("pages:extend", (pages: NuxtPage[]) => {
+		const toRemove = []
+		for (const page of pages) {
+			const isWanted = routesToInclude.some(route => route === page.path)
+			if (!isWanted) {
+				toRemove.push(page)
+			}
 		}
-	}
-	for (const page of toRemove) pages.splice(pages.indexOf(page), 1)
+		for (const page of toRemove) pages.splice(pages.indexOf(page), 1)
+	})
 }
 
