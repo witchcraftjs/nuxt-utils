2246b69991dc85a9ebe98f365f71c92ff1c7065f
Alan North <alanscodelog@gmail.com>
2025-06-15 11:42:37 -0300

    v0fix: fixed nuxtRerouteOutputTo not working

--- src/runtime/utils/nuxtRerouteOutputTo.ts
+++ src/runtime/utils/nuxtRerouteOutputTo.ts
@@ -1,17 +1,13 @@
-import { type NuxtConfig } from "nuxt/schema"
+import {  type Nuxt } from "nuxt/schema"
 import path from "path"
 
 /** Changes the build output dir so the build doesn't interfere with other builds. */
-export function nuxtRerouteOutputTo(dir: string) {
-	return {
-		nitro: {
-			output: {
-				dir: path.join(dir),
-				// we don't really care about the server for static builds, but it would otherwise overwrite our web build dir
-				serverDir: path.join(dir, "server"),
-				publicDir: path.join(dir, "public"),
-			},
-		},
-	} satisfies NuxtConfig
+export function nuxtRerouteOutputTo(nuxt: Nuxt, dir: string): void {
+	nuxt.hook("nitro:config", config => {
+		config.output ??= {}
+		config.output.dir = path.join(dir)
+		config.output.serverDir = path.join(dir, "server")
+		config.output.publicDir = path.join(dir, "public")
+	})
 }
 
