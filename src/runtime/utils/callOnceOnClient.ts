e410671d1ea617f14eb8e57bd36fe484160b1ba5
Alan North <alanscodelog@gmail.com>
2026-07-09 23:07:57 -0300

    docs: documented callOnceOnClient

--- src/runtime/utils/callOnceOnClient.ts
+++ src/runtime/utils/callOnceOnClient.ts
@@ -1,5 +1,10 @@
 import { callOnce } from "#app"
 
+/**
+ * Intended to help with things like initializations on the client (e.g. databases, stores, etc.)
+ *
+ * Note that if your imports have side effect, this function won't save you. Easiest solution in nuxt is to place this function in a client-only component, e.g. Setup.client.vue. Makes it nice and easy to wrap around the client only code that actually needs the initializations. Alternatively use async imports.
+ */
 export async function callOnceOnClient<T extends (...args: any[]) => any>(
 	key: string,
 	fn: T
